0e07430b9c705a1b6ac1230c1ec466e2be603777
nikilr2 <nikilr2@illinois.edu>
2019-12-03 15:38:11 -0600

    Changed title and style

--- app/src/main/java/com/example/cs125finalproject/DisplayActivity.java
+++ app/src/main/java/com/example/cs125finalproject/DisplayActivity.java
@@ -1,16 +1,43 @@
 package com.example.cs125finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
-public class DisplayActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.OnMapReadyCallback;
+//import com.google.android.gms.maps.SupportMapFragment;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
+
+//package com.example.mapwithmarker;
+
+
+public class DisplayActivity extends AppCompatActivity { //implements OnMapReadyCallback {
     private static final String TAG = "DisplayMapActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
         Log.i(TAG, "Map activity has been triggered");
+        //setContentView(R.layout.activity_maps);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        //        .findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
     }
+    /*
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    */
 }
--- app/src/main/java/com/example/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.cs125finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public final class MainActivity extends AppCompatActivity {
 
@@ -16,6 +16,10 @@ public final class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //For all below code, should we finish or not??
+        TextView title = findViewById(R.id.title);
+        title.setTextColor(Color.WHITE);
+        title.setBackgroundColor(Color.CYAN);
+
 
         Button eventsmap = findViewById(R.id.mapButton);
         //context must be checked
